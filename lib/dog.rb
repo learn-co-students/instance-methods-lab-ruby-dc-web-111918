5eb705b42377cb51d3ced95a3376a21d4ea1e173
github username <github email address>
2018-11-29 08:24:29 -0500

    instance methods lab complete

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+#instance method bark
+  def bark
+    puts "Woof!"
+  end
+#instance method sit
+  def sit
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+#instance method talk
+  def talk
+    puts "Hello World!"
+  end
+#instance method walk
+  def walk
+    puts "The Person is walking"
+  end
+end
